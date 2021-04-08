efa082ee6407e03b5cf562c2bbfd4c7abd87e27c
Marek Gajda <marektychy94@wp.pl>
2021-04-08 19:41:15 +0200

    Add project files.

---
+++ Zad1_ProgZaaw_MarekGajda/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Zad1_ProgZaaw_MarekGajda.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
